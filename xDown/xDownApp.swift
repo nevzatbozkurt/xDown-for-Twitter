dc24232d8feecd2001912970b533a87e46fdad92
Nevzat BOZKURT <bozkurtnevzat@yahoo.com.tr>
2023-11-16 11:51:58 +0300

    Initial Commit

---
+++ xDown/xDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  xDownApp.swift
+//  xDown
+//
+//  Created by Nevzat BOZKURT on 16.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct xDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
